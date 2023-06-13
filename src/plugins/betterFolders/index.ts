49c37eff0328fdc5bfcc9bbc7c1b188a1be3cd09
gracefulLion <38409600+Redundakitties@users.noreply.github.com>
2023-06-13 17:11:13 -0400

    removed unnecesary code

--- src/plugins/betterFolders/index.ts
+++ src/plugins/betterFolders/index.ts
@@ -23,10 +23,8 @@ import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { findByPropsLazy, findLazy, findStoreLazy } from "@webpack";
 import { FluxDispatcher } from "@webpack/common";
-import FolderSideBar from "./FolderSideBar";
 
-let isEscapePressed = false;
-const keydown = (e: KeyboardEvent) => e.key === "Escape" && (isEscapePressed = true);
+import FolderSideBar from "./FolderSideBar";
 
 const GuildsTree = findLazy(m => m.prototype?.convertToFolder);
 const GuildFolderStore = findStoreLazy("SortedGuildStore");
